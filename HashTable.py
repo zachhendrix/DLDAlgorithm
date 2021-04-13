9fbf1a4e302c8c544cdce084f818de14b03fcb3b
zachhendrix <51380493+zachhendrix@users.noreply.github.com>
2021-04-13 11:50:27 -0500

    Initial commit

---
+++ HashTable.py
@@ -0,0 +1,6 @@
+import csv
+
+with open(.)
+
+
+packageList ={'ID': '0', 'Address': '', 'Deadline': '', 'City': '', 'Zipcode': '', 'Weight': '', 'Status': ''}
---
+++ main.py
@@ -0,0 +1,13 @@
+# Zach Hendrix Student ID:001220147
+import HashTable
+
+
+def main_menu():
+    print('')
+    print('Welcome to the Daily Local Delivery List for WGU')
+    print('Please input a command and press the "Enter" key:')
+    print('1. Print Deliveries')
+    print('2. Lookup Specific Deliveries')
+    print('3. Lookup Delivery Status')
+    print('4. Lookup Truck Status')
+    input()
