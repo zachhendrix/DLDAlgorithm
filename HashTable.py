69dcc15ea0f1b726f69b37c7b73b769360545bf8
zachhendrix <51380493+zachhendrix@users.noreply.github.com>
2021-04-13 12:01:40 -0500

    Update HashTable.py

--- HashTable.py
+++ HashTable.py
@@ -1,6 +1,8 @@
 import csv
 
-with open(.)
+with open('./projectdata/WGUPS Package File.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
 
 
 packageList ={'ID': '0', 'Address': '', 'Deadline': '', 'City': '', 'Zipcode': '', 'Weight': '', 'Status': ''}
