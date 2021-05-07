f10e5131ffac5ec90a994841e749eb21413a84d2
zachhendrix <zach.hendrix@yahoo.com>
2021-05-07 15:01:08 -0500

    0.3.1

--- Truck.py
+++ Truck.py
@@ -1,1 +1,14 @@
 class Truck:
+    mileage = 0
+    miles_per_min = .3
+    cargo = {}
+
+    def __init__(self, mlg, mpm, carg):
+        self.mileage = mlg
+        self.miles_per_min = mpm
+        self.cargo = carg
+
+    def display(self):
+        print("First number = " + str(self.first))
+        print("Second number = " + str(self.second))
+        print("Addition of two numbers = " + str(self.answer))
